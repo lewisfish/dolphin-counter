dbfa36b6d8679b7f193079c68db12e186edbb158
lewis <lewistmcmillan@gmail.com>
2020-01-16 09:45:56 +0000

    tidy up and commented getFrames.py

--- getFrames.py
+++ getFrames.py
@@ -1,9 +1,23 @@
 import cv2
-import subprocess
 from argparse import ArgumentParser
 
 
 def getTimes(file):
+    '''Function takes a filename and returns times in seconds from an expected
+       format of hr:min:sec
+
+    Parameters
+    ----------
+
+    file : str
+        Name of file to open and process.
+
+    Returns
+    -------
+
+    times : List[float]
+        List of times to capture frames.
+    '''
 
     times = []
 
@@ -27,24 +41,20 @@ parser.add_argument("-t", "--times", type=str,
 
 args = parser.parse_args()
 
-
-out = subprocess.Popen(["ffmpeg", "-i", args.file], stdout=subprocess.PIPE,
-                       stderr=subprocess.STDOUT)
-
-stdout, stderr = out.communicate()
-
+# open video file
 cap = cv2.VideoCapture(args.file)
-fps = cap.get(cv2.CAP_PROP_FPS)
-totalFrameNumber = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+fps = cap.get(cv2.CAP_PROP_FPS)  # get fps
 
 times = getTimes(args.times)
 
+# loop over times to create frames
 for i, time in enumerate(times):
+    # get frame number from fps and timestamp in seconds
     frameNum = int(time * fps)
+    # set position in video as frameNum
     cap.set(cv2.CAP_PROP_POS_FRAMES, frameNum)
     _, frame = cap.read()
+    # save frame as png
     name = args.file[0:13] + str(frameNum)
     print(name + ".png")
     cv2.imwrite(name + ".png", frame)
