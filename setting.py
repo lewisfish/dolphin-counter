866ae121693e9d4cf6174feb35f8c1236e5f4e7b
lewis <lewistmcmillan@gmail.com>
2020-02-07 16:44:16 +0000

    Added some code to limit memory usage on gpu when running tensorflow

--- setting.py
+++ setting.py
@@ -1,2 +1,10 @@
 import tensorflow as tf
+# make sure there is a gpu available and set it such that it dynamically
+# allocates memory
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+# assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
 model = tf.keras.models.load_model('Ml-test/image_to_number_model.hdf5')
