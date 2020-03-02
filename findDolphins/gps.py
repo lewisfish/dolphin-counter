7d37f6a30a012cb6449f9f1afd52ffb8750f2739
lewis <lewistmcmillan@gmail.com>
2020-03-02 14:27:57 +0000

    Update renderVideo.py. Added python script to get gps data (altitude) gps.py

---
+++ findDolphins/gps.py
@@ -0,0 +1,167 @@
+import bisect
+import datetime
+from pathlib import Path
+from typing import Generator, Tuple, List
+
+import cv2
+import numpy as np
+import pandas as pd
+
+__all__ = ["getAltitude"]
+
+
+def _getTimeDate(filename: Path) -> datetime.datetime:
+    '''Function gets the date and video start time from video files name
+
+    Parameters
+    ----------
+
+    filename : Path object
+        Path to the video file
+
+    Returns
+    -------
+
+    dt : datetime.datetime object
+        The date and time of the drones flight start
+
+    '''
+    # remove folders and codec from filename
+    name = filename.stem
+
+    date = name[:10].split("_")
+    year = int(date[0])
+    month = int(date[1])
+    day = int(date[2])
+
+    time = name[11:].split("_")
+    hour = int(time[0])
+    minute = int(time[1])
+    second = int(time[2])
+    millsecond = int(time[3])
+    dt = datetime.datetime(year, month, day, hour, minute, second, millsecond)
+
+    return dt
+
+
+def _findStart(files: Generator[Path, None, None], targetTime: datetime.datetime) -> Tuple[Path, List, List, int]:
+    '''Function matches the videos to a drone flight number. Also matches start
+       time to the gps data which starts recording before the video records,
+       and has a different tic from that of the video.
+
+    Parameters
+    ----------
+
+    files : Generator object
+        Generator object that yields the file names of the possible GPS data
+        files.
+
+    targetTime : datetime.datetime object
+        The time to search for in the GPS data.
+
+    Returns
+    -------
+
+    outfile, outalts, outtimes, curPosition : Tuple(Path, List, List, int)
+        Outfile is the GPS-data file filename : Path or str
+        outalts is a list of altitudes for the given video.
+        outtimes is a list of times for the given video where GPS was recorded.
+        curPosition is the location in the list of times/GPS data when the
+        video feed begins.
+
+    '''
+
+    curPosition = 1e99
+    for file in files:
+        times = []
+        df = pd.read_csv(file)
+        time = df["Time"]
+
+        # format first and last datetime and check if target falls between them.
+        # as strptime is reallllllyyyy slow...
+        t = time.iloc[0]
+        fmttime = datetime.datetime.strptime(t, "%Y-%m-%dT%H:%M:%S.%f")
+        times.append(fmttime)
+
+        t = time.iloc[-1]
+        fmttime = datetime.datetime.strptime(t, "%Y-%m-%dT%H:%M:%S.%f")
+        times.append(fmttime)
+
+        # find position of target time in times array
+        position = bisect.bisect_left(times, targetTime)
+
+        # if position not in list then it returns the length of the list as the position
+        if position == 1 and position < curPosition:
+            outfile = file
+            curPosition = position
+            outtimes = times.copy()
+            outalts = df["Altitude/mm"]
+
+    return outfile, outalts, outtimes, curPosition
+
+
+def getAltitude(videoFile: str, framenumber: int, gpsdataPath="videos+data/gps-data/") -> float:
+    '''Function calculates the altitude of the drone given a video and a frame
+       number from that video to match the GPS data to.
+
+    Parameters
+    ----------
+
+    videoFile : Path object or str
+        Path to the video file for which an altitude is required.
+
+    framenumber : int
+        Number of the frame for which the altitude is required.
+
+    gpsdataPath : str, optional
+        Path to GPS data folder
+
+    Returns
+    -------
+
+    currentAltitude : float
+        The calculated altitude.
+
+    '''
+
+    # open and set video to frame of interest, whilst collecting
+    # metadata from video
+
+    videoPath = Path(videoFile)
+    cap = cv2.VideoCapture(str(videoFile))  # converts to BGR by default
+    length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, framenumber)
+
+    # get current position of video in msec from start
+    msec = cap.get(cv2.CAP_PROP_POS_MSEC)
+    msec /= 1e3
+    _, frame = cap.read()
+
+    cap.release()
+
+    dt = _getTimeDate(videoFile) + datetime.timedelta(seconds=3)  # adjust for video recording delay
+    # form gpsdata filename
+    date = str(dt.year) + str(dt.month) + str(dt.day)
+    path = Path(gpsdataPath)
+    files = path.glob(date + "*.csv")
+    file, alts, times, pos = _findStart(files, dt)
+
+    # get the altitude
+    frameTime = dt + datetime.timedelta(seconds=msec)
+    position = bisect.bisect_left(times, frameTime)
+    currentTime = times[position-1]
+    currentAltitude = alts[position-1]
+
+    return currentAltitude
+
+
+if __name__ == '__main__':
+
+    # given a frame number what is the altitude?
+    altsa = []
+    cnt = 0
+    for i in range(0, 80000, 5000):
+        altitude = getAltitude(Path("videos+data/2019_11_23_16_16_02_506.mp4"), i)
+        altsa.append(altitude / 1e3)
+        cnt += 1
--- findDolphins/renderVideo.py
+++ findDolphins/renderVideo.py
@@ -7,7 +7,7 @@ from scipy import signal
 from argparse import ArgumentParser
 
 
-def createDict(filename):
+def createDict(filename: str):
 
     f = open(filename, "r")
     lines = f.readlines()
@@ -37,76 +37,78 @@ def createDict(filename):
     return mydict
 
 
-parser = ArgumentParser(description="Render video from output of dolphin detection.")
+if __name__ == '__main__':
 
-parser.add_argument("-f", "--file", type=str,
-                    help="Path to output file to be analysed.")
+    parser = ArgumentParser(description="Render video from output of dolphin detection.")
 
-parser.add_argument("-v", "--video", type=str,
-                    help="Path to video file to be cut up.")
+    parser.add_argument("-f", "--file", type=str,
+                        help="Path to output file to be analysed.")
 
-# parser.add_argument("-d", "--debug", action="count", default=0,
-#                     help="Display debug info.")
-parser.add_argument("-pt", "--plot", action="store_true",
-                    help="Display plot of dolphin count over all frames.")
-parser.add_argument("-nv", "--novideo", action="store_true",
-                    help="If provided do not render video.")
+    parser.add_argument("-v", "--video", type=str,
+                        help="Path to video file to be cut up.")
 
-args = parser.parse_args()
+    # parser.add_argument("-d", "--debug", action="count", default=0,
+    #                     help="Display debug info.")
+    parser.add_argument("-pt", "--plot", action="store_true",
+                        help="Display plot of dolphin count over all frames.")
+    parser.add_argument("-nv", "--novideo", action="store_true",
+                        help="If provided do not render video.")
 
-file = args.file
-genny = createDict(file)
+    args = parser.parse_args()
 
-videoFile = args.video
-genny = {k: OrderedDict(sorted(v.items())) for k, v in genny.items()}
+    file = args.file
+    genny = createDict(file)
 
-if not args.novideo:
-    cap = cv2.VideoCapture(videoFile)  # converts to BGR by default
-    cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-    _, frame = cap.read()
-    h, w, layers = frame.shape
-    writer = cv2.VideoWriter("output-new-1.avi", cv2.VideoWriter_fourcc(*"XVID"), 1, (w, h))
+    videoFile = args.video
+    genny = {k: OrderedDict(sorted(v.items())) for k, v in genny.items()}
 
-i = 0
-dolphinCount = []
-for video in genny:
-    for time in genny[video]:
+    if not args.novideo:
+        cap = cv2.VideoCapture(videoFile)  # converts to BGR by default
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        _, frame = cap.read()
+        h, w, layers = frame.shape
+        writer = cv2.VideoWriter("output-new-1.avi", cv2.VideoWriter_fourcc(*"XVID"), 1, (w, h))
 
-        if not args.novideo:
-            cap.set(cv2.CAP_PROP_POS_FRAMES, time)
-            _, frame = cap.read()
+    i = 0
+    dolphinCount = []
+    for video in genny:
+        for time in genny[video]:
 
-        numDolphins = 0
-        for bbox in genny[video][time]:
-
-            x1 = int(bbox[0][1])
-            x2 = int(bbox[1][1])
-            y1 = int(bbox[0][0]) + 130
-            y2 = int(bbox[1][0]) + 130
             if not args.novideo:
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            numDolphins += 1
-        dolphinCount.append(numDolphins)
-        # plt.imshow(frame)
-        # plt.show()
-        if not args.novideo:
-            org = (50, 1040-100)
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            fontScale = 1
-            color = (255, 0, 0)
-            thickness = 2
-            cv2.putText(frame, f"{numDolphins}", org, font,
-                        fontScale, color, thickness, cv2.LINE_AA)
-            # frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-            writer.write(frame.astype("uint8"))
-        print(i)
-        i += 1
+                cap.set(cv2.CAP_PROP_POS_FRAMES, time)
+                _, frame = cap.read()
 
-if args.plot:
-    plt.plot(dolphinCount)
-    plt.show()
+            numDolphins = 0
+            for bbox in genny[video][time]:
 
-if not args.novideo:
-    cv2.destroyAllWindows()
-    cap.release()
-    writer.release()
+                x1 = int(bbox[0][1])
+                x2 = int(bbox[1][1])
+                y1 = int(bbox[0][0]) + 130
+                y2 = int(bbox[1][0]) + 130
+                if not args.novideo:
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                numDolphins += 1
+            dolphinCount.append(numDolphins)
+            # plt.imshow(frame)
+            # plt.show()
+            if not args.novideo:
+                org = (50, 1040-100)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+                cv2.putText(frame, f"{numDolphins}", org, font,
+                            fontScale, color, thickness, cv2.LINE_AA)
+                # frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+                writer.write(frame.astype("uint8"))
+            print(i)
+            i += 1
+
+    if args.plot:
+        plt.plot(dolphinCount)
+        plt.show()
+
+    if not args.novideo:
+        cv2.destroyAllWindows()
+        cap.release()
+        writer.release()
