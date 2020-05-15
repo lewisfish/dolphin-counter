053b486974d79431a4025912da016fd88821b118
lewis <lewistmcmillan@gmail.com>
2020-05-15 12:11:59 +0100

    Added dolphin dataset loader.

--- ML/customdata.py
+++ ML/customdata.py
@@ -5,6 +5,8 @@ import numpy as np
 import torch
 from torchvision import transforms
 
+__all__ = ["OIDdataset", "DolphinDataset"]
+
 
 class OIDdataset(object):
     """docstring for OIDdataset"""
@@ -93,6 +95,89 @@ class OIDdataset(object):
         target["image_id"] = torch.as_tensor(idx, dtype=torch.int64)
         if self.transform:
             image, target = self.transform(image, target)
+
+        return image, target
+
+    def __len__(self):
+        return len(self.labels)
+
+# 錄製_2019_11_20_10_13_54_900.mp4, 43472, 229, 885, 235, 896, 10
+# 錄製_2019_11_29_16_14_59_189.mp4, 31677, 127, 606, 141, 658, 10
+# 錄製_2019_11_20_10_13_54_900.mp4, 44691, 88, 1325, 93, 1332, 10
+# 錄製_2019_11_28_12_05_07_124.mp4, 30440, 749, 550, 758, 556, 10
+
+# filename, framenumber, x0, y0, x1, y1, label
+# cavet is that y1 and y2 offset by 130 due to cropping of screen recording
+# this wont be true for all data after deploy though
+# just true of the train, test, validation sets.
+
+
+class DolphinDataset(object):
+    """docstring for DolphinDataset"""
+    def __init__(self, root, transforms, file):
+        super(DolphinDataset, self).__init__()
+        self.root = Path(root)
+        self.transforms = transforms
+        self.datafile = file
+        self.videoFiles = list(self.root.glob("**/*.mp4"))
+
+        self.labels = []
+        self.frameNumbers = []
+        self.bboxs = []
+        self.videoFileNames = []
+
+        # load label file into memory
+        with open(self.datafile, "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                parts = line.split(",")
+                videoName = self._getFullFileName(parts[0])
+                self.videoFileNames.append(videoName)
+                self.frameNumbers.append(int(parts[1]))
+                self.bboxs.append([int(parts[2]), int(parts[3]), int(parts[4]), int(parts[5])])
+                self.labels.append(int(parts[6]))
+
+    def _getFullFileName(self, target):
+        '''Get the full filename path'''
+
+        for file in self.videoFiles:
+            if target in str(file):
+                return file
+
+    def __getitem__(self, idx):
+        print(self.videoFileNames[idx])
+        cap = cv2.VideoCapture(str(self.videoFileNames[idx]))  # converts to RGB by default
+        cap.set(cv2.CAP_PROP_POS_FRAMES, self.frameNumbers[idx])
+        _, image = cap.read()
+        cap.release()
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)/255.0
+        ymax, xmax = image.shape[0], image.shape[1]
+
+        target = {}
+
+        # y0, x0, y1, x1
+        # boxes = [[self.bboxs[idx][1]/xmax, (self.bboxs[idx][0]+130)/ymax, self.bboxs[idx][3]/xmax, (self.bboxs[idx][2]+130)/ymax]]
+        left = self.bboxs[idx][0]
+        top = self.bboxs[idx][1] + 130
+        right = self.bboxs[idx][2]
+        bottom = self.bboxs[idx][3] + 130
+        bbox = [left, top, right, bottom]
+        bbox = torch.as_tensor([bbox], dtype=torch.float32)
+
+        area = np.abs(right - left) * np.abs(bottom - top)
+
+        label = self.labels[idx] + 1  # as 0 is background
+        label = torch.as_tensor([label], dtype=torch.int64)
+
+        target["boxes"] = torch.as_tensor(bbox, dtype=torch.float32)
+        target["labels"] = torch.as_tensor(label, dtype=torch.int64)
+        target["area"] = torch.as_tensor([area], dtype=torch.float32)
+        target["iscrowd"] = torch.as_tensor(0, dtype=torch.int64)
+        target["image_id"] = torch.as_tensor(idx, dtype=torch.int64)
+
+        if self.transforms:
+            image, target = self.transforms(image, target)
 
         return image, target
 
